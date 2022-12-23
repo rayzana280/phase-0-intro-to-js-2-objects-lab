6046679e7b189f28dff4dd3273c9839a0be0d64c
Ray Zana <rayzana280@gmail.com>
2022-12-23 01:06:40 -0500

    commit tree fc3ef8d46e93049190fbd155d54ca7b72352bff2

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name : 'ray',
+    streetAddress : '101 street'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employee1 = {...employee}
+    employee1[key] = value 
+    return employee1
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value 
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+//make a copy of object 
+const employee2 = {...employee}
+//delete key from new object 
+delete employee2[key];
+//return new object 
+return employee2;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee;
+}
